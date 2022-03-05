1d8db531774c4e45259af75cc9889946932e2f7b
itsvvill <w.lewis.eason@gmail.com>
2022-03-04 17:12:57 -0800

    added basic structure for styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
- 
-function App() {
+import React from "react";
+//Components
+import Header from "./components/Header";
+
+//Styles
+import { GlobalStyle } from "./GlobalStyle";
+
+const App = () => {
   return (
     <div className="App">
+      <Header />
       Start here.
+      <GlobalStyle />
     </div>
   );
-}
+};
 
 export default App;
