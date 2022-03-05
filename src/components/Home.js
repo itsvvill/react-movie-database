5eff193f6330a7d780026aaf5a75189e323c72aa
itsvvill <w.lewis.eason@gmail.com>
2022-03-05 14:25:58 -0800

    added logic for fetching movie results

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,43 @@
-import React, { useState } from "react";
-//Config
+import React, { useState, useEffect } from "react";
+// API
+import API from "../API";
+// Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
-//Components
+// Components
 
-//Hook
+// Hook
 
-//Image
+// Image
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  // Initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
 
   return <div>Home Page</div>;
 };
