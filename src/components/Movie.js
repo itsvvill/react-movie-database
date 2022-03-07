b8b797a5fbdc35a1b7885455213cc94f2030c7b8
itsvvill <w.lewis.eason@gmail.com>
2022-03-07 12:18:25 -0800

    added custom hook for fetching movie data

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,21 @@
 import React from "react";
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+// Components
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+// Hook
+
+// Image
+import NoImage from "../images/no_image.jpg";
+import { Routes } from "react-router";
 
 const Movie = () => {
-  return <div>movie</div>;
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  );
 };
 
 export default Movie;
