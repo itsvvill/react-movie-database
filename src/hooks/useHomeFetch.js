1f72b15425f7794f15ed9a66c291381fefe1e0f5
itsvvill <w.lewis.eason@gmail.com>
2022-03-08 12:03:33 -0800

    added session storage for home page

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 // API
 import API from "../API";
+// Helpers
+import { isPersistedState } from "../helpers";
 
 const initialState = {
   page: 0,
@@ -36,6 +38,15 @@ export const useHomeFetch = () => {
 
   // Initial render and search
   useEffect(() => {
+    if (!searchTerm) {
+      const sessionState = isPersistedState("homeState");
+
+      if (sessionState) {
+        setState(sessionState);
+        return;
+      }
+    }
+
     setState(initialState);
     fetchMovies(1, searchTerm);
   }, [searchTerm]);
@@ -47,6 +58,11 @@ export const useHomeFetch = () => {
     fetchMovies(state.page + 1, searchTerm);
     setIsLoadingMore(false);
   }, [isLoadingMore, searchTerm, state.page]);
+
+  // Write to sessionStorage
+  useEffect(() => {
+    if (!searchTerm) sessionStorage.setItem("homeState", JSON.stringify(state));
+  }, [searchTerm, state]);
 
   return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 };
