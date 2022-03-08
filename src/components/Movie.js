ac24871a014582453683d8c0658ce6bf2eec2dac
itsvvill <w.lewis.eason@gmail.com>
2022-03-07 19:21:53 -0800

    added movie info and info bar components

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,6 +6,8 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb";
+import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
 // Image
@@ -23,6 +25,12 @@ const Movie = () => {
   return (
     <>
       <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
     </>
   );
 };
